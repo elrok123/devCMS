609b3f9badb1a0bd8369f5a1b0d2f88c3f12dbdb
Conner Stephen McCabe <conner@koansystems.co.uk>
2014-11-06 10:42:17 +0000

    Update index.php
    Removed old hash salt

--- hasher/index.php
+++ hasher/index.php
@@ -3,6 +3,6 @@
 <form method="GET" name="form1">
 
 	<input type="text" name="unhashed" value="Unhashed"></br>
-	<textarea><?php if(isset($_GET['unhashed'])){echo crypt($_GET['unhashed'], "\$iLikePotatoes\$");}else{echo "Hash";} ?></textarea>
+	<textarea><?php if(isset($_GET['unhashed'])){echo crypt($_GET['unhashed'], "\$*************\$");}else{echo "Hash";} ?></textarea>
 	<input type="submit" />
 </form>
